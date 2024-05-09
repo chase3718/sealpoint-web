c85b95f285d7bfa24c9d0b2da63aecc6b7295836
Chase Williams <chasewilliams98@gmail.com>
2024-05-09 18:41:09 -0400

    moved socials

--- app/page.js
+++ app/page.js
@@ -7,10 +7,10 @@ import Spotify from './components/Spotify/spotify';
 export default function Home() {
 	return (
 		<div className={styles.main}>
-			<Socials />
 			<div className={styles.logo}>
 				<Image src="/Logo_Red.svg" alt="logo" width={0} height={0} style={{ width: '100%', height: '100%' }} priority />
 			</div>
+			<Socials />
 			{/* <WhoAreWe /> */}
 			<Spotify />
 		</div>
