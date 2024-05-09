b3edfe2ba77782e21e799322ec44ebbb6bc73cff
Chase Williams <chasewilliams98@gmail.com>
2024-05-09 09:53:18 -0400

    add futura as main font

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google';
+import localFont from '@next/font/local';
 import './globals.css';
 import Navbar from './components/Navbar/navbar';
+import Footer from './components/Footer/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,12 +11,19 @@ export const metadata = {
 	description: 'The official website of Seal Point',
 };
 
+const futura = localFont({
+	src: [{ path: '../public/font/Futura Heavy font.ttf', format: 'truetype' }],
+	display: 'swap',
+	variable: 'Futura',
+});
+
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={futura.className}>
 			<body>
 				<Navbar />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
